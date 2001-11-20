bd7abca9b0bcad6ec1a2e6c602ed81247b83b440
alexgn <alexgn>
2001-11-20 12:52:14 +0000

    Seite auf der man angibt welche Sprachen man beherrscht

---
+++ prototyp/user/signup2a.php
@@ -0,0 +1,129 @@
+<? /*
+    * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/prototyp/user/signup2a.php,v $ 
+    * $Revision: 1.1 $
+    * $Id: signup2a.php,v 1.1 2001/11/20 12:52:14 alexgn Exp $
+    */
+?>
+<html>
+<head>
+<title>otmp - signup</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="description" content="OTMP - Online Translation Marketplace
+Signup">
+<link rel="stylesheet" href="../otmp.css">
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="1" topmargin="1">
+<table width="100%" border="0" cellspacing="0" cellpadding="2">
+  <tr bgcolor="#666666"> 
+    <td colspan="2"> 
+      <div align="right"><b><font color="#FFFFFF">you are not logged in</font></b></div>
+    </td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><b class="logo">OTMP</b></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr> 
+    <td colspan="2"> 
+      <div align="right"><a href="../main.php">home</a> | <a href="../show.php">texts</a> 
+        | <a href="options.php">options</a> | <a href="login.php">login</a></div>
+    </td>
+  </tr>
+</table>
+<h3 align="left">Anmeldung als &Uuml;bersetzer</h3>
+<div align="center"><br>
+  Im zweiten Schritt der Anmeldung m&uuml;ssen Sie angeben welche Sprachen Sie beherrschen.<br>
+  <br><br>
+  <b>Schritt 2</b>
+</div>
+<form method="post" action="signup3a.php">
+  <div align="center"> 
+    <table cellspacing="0" cellpadding="1" bgcolor="#000000">
+      <tr> 
+        <td> 
+          <table cellspacing="0" cellpadding="6">
+            <tr bgcolor="#FFFFFF"> 
+              <td class="label" colspan="3" align="center">Sie k&ouml;nnen &uuml;bersetzen von </td>
+            </tr>
+            <tr bgcolor="#FFFFFF"> 
+              <td class=label>
+		<p>
+		  <select name="sprache1" size="5" multiple>
+		    <option>Arabisch</option>
+		    <option>Chinesisch (Mandarin)</option>
+		    <option>D&auml;nisch</option>
+		    <option>Englisch</option>
+		    <option>Finnisch</option>
+		    <option>Franz&ouml;sisch</option>
+		    <option>Italienisch</option>
+		    <option>Kroatisch</option>
+		    <option>Norwegisch</option>
+		    <option>Polnisch</option>
+		    <option>Portugiesisch</option>
+		    <option>Rum&auml;nisch</option>
+		    <option>Russisch</option>
+		    <option>Schwedisch</option>
+		    <option>Serbisch</a>
+		    <option>Slovakisch</option>
+		    <option>Spanisch</option>
+		    <option>Tschechisch</option>
+		    <option>T&uuml;rkisch</option>
+		    <option>Ungarisch</option>	
+		  </select>
+		</p>
+	      </td>
+              <td class="label" align="center"> 
+                nach
+              </td>
+	      <td class="label">
+		<p>
+		  <select name="sprache1" size="5" multiple>
+		    <option>Arabisch</option>
+		    <option>Chinesisch (Mandarin)</option>
+		    <option>D&auml;nisch</option>
+		    <option>Englisch</option>
+		    <option>Finnisch</option>
+		    <option>Franz&ouml;sisch</option>
+		    <option>Italienisch</option>
+		    <option>Kroatisch</option>
+		    <option>Norwegisch</option>
+		    <option>Polnisch</option>
+		    <option>Portugiesisch</option>
+		    <option>Rum&auml;nisch</option>
+		    <option>Russisch</option>
+		    <option>Schwedisch</option>
+		    <option>Serbisch</a>
+		    <option>Slovakisch</option>
+		    <option>Spanisch</option>
+		    <option>Tschechisch</option>
+		    <option>T&uuml;rkisch</option>
+		    <option>Ungarisch</option>	
+		  </select>
+		</p>
+	      </td>
+            </tr>
+            <tr bgcolor="#FFFFFF" > 
+              <td colspan="3" align="center"> 
+                <input type="submit" name="signup" value="Weiter">
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </div>
+</form>
+<b>Hilfe:</b><br>
+<i>Wie kann ich mehrere Sprachen auswählen?</i><br><br>
+Dies ist leider von Browser zu Browser unterschiedlich.<br>
+ Bei Netscape 4.x wählen Sie mit der Maus einfach weitere Sprachen aus. Klicken Sie ein zweites Mal auf einen selektierten Eintrag, dann ist er nicht mehr selektiert.<br>
+Beim IE müssen Sie die <KBD>Schift</KBD> Taste gedrückt halten und dann mit der Maus weitere Elemente auswählen.<br>
+<p align="center" class="footer"><a href="../main.php"> home</a>| <a href="../show.php">texts</a> 
+  | <a href="../upload.php">upload</a> | <a href="../contact.php">contact</a> 
+  | <a href="../help.php">help</a><br>
+  <a href="login.php">login</a><br>
+  <br>
+  $Revision: 1.1 $ &copy; 2001 otmp-developer :)</p>
+</body>
+</html>
