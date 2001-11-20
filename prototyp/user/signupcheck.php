d9af9eda527790df00db63aa9cfa81751ad9e14c
alexgn <alexgn>
2001-11-20 12:47:58 +0000

    Dies ist die Seite wo man seine Daten nochmal überprüfen kann ehe man sie abschickt

---
+++ prototyp/user/signupcheck.php
@@ -0,0 +1,164 @@
+<? /*
+    * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/prototyp/user/signupcheck.php,v $ 
+    * $Revision: 1.1 $
+    * $Id: signupcheck.php,v 1.1 2001/11/20 12:47:58 alexgn Exp $
+    */
+?>
+<html>
+<head>
+<title>otmp - signup</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="description" content="OTMP - Online Translation Marketplace
+Signup">
+<link rel="stylesheet" href="../otmp.css">
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="1" topmargin="1">
+<table width="100%" border="0" cellspacing="0" cellpadding="2">
+  <tr bgcolor="#666666"> 
+    <td colspan="2"> 
+      <div align="right"><b><font color="#FFFFFF">you are not logged in</font></b></div>
+    </td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td><b class="logo">OTMP</b></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr> 
+    <td colspan="2"> 
+      <div align="right"><a href="../main.php">home</a> | <a href="../show.php">texts</a> 
+        | <a href="options.php">options</a> | <a href="login.php">login</a></div>
+    </td>
+  </tr>
+</table>
+<h3 align="left">Anmeldung als &Uuml;bersetzer</h3>
+<div align="center"><br>
+  Auf dieser Seite bitten wir Sie Ihre Daten auf Richtigkeit zu überprüfen, falls alles richtig ist, drücken sie bitte auf "Daten absenden" andernfalls auf "Daten ändern".<br>
+  <br>
+</div>
+<div align="left"><br>
+  <table border="0" cellpadding="1">
+    <tr>
+       <td colspan="2"><b><font size="3">Angaben zu Ihrer Person</font></b><br>
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1"><b>Name</b>
+       </td>
+       <td>Schmitz
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1"><b>Vorname</b>
+       </td>
+       <td>Rudolph
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1"><b>E-mail</b>
+       </td>
+       <td>Schmitz@t-online.de
+       </td>
+    </tr>
+    <tr>
+	<td height="10"></td>
+    </tr>
+    <tr>
+       <td colspan="2"><b><font size="3">Angaben zu Ihren Sprachkenntnissen</font></b><br>
+       </td>
+    </tr>
+    <tr>
+       <td colspan="2">Sie k&ouml;nnen &uuml;bersetzen</td>
+    </tr>
+    <tr>
+       <td colspan="1"><b>Von</b>
+       </td>
+       <td><b>Nach</b>
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1">Englisch
+       </td>
+       <td>Deutsch
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1">Dänisch
+       </td>
+       <td>Deutsch
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1">Russisch
+       </td>
+       <td>Deutsch
+       </td>
+    </tr>
+    <tr>
+       <td colspan="1">Schwedisch
+       </td>
+       <td>Deutsch
+       </td>
+    </tr>
+    <tr>
+	<td height="10"></td>
+    </tr>
+    <tr>
+       <td colspan="2"><b><font size="3">Angaben zu Ihren Fachgebieten</font></b><br>
+       </td>
+    </tr>
+    <tr>
+       <td valign="top"><b>Geisteswissenschaften</b></td>
+       <td>Geschichte<br>
+	   Philosophie<br>
+           Sonstige Geisteswissenschaften
+       </td>
+    </tr>
+    <tr>
+       <td valign="top"><b>Medizin</b></td>
+       <td>Medizin</td>
+    </tr>
+    <tr>
+       <td>
+    </tr>
+     <tr>
+	<td height="10"></td>
+    </tr>
+    <tr>
+       <td colspan="2"><b><font size="3">Folgende Resourcen stehen Ihnen zur Verfügung</font></b><br>
+       </td>
+    </tr>
+    <tr>
+       <td valign="top"><b>Programme</b></td>
+       <td>StarOffice<br>
+	   Emacs
+       </td>
+    </tr>
+    <tr>
+       <td valign="top"><b>Formate</b></td>
+       <td>HTML<br>
+	   ASCII Text<br>
+           Sonstige
+       </td>
+    </tr>  
+    <tr>
+       <td valign="top"><b>Packer</b></td>
+       <td>keine<br></td>   
+    </tr>
+    <tr>
+	<td height="15"></td>
+    </tr>
+    <tr>
+       <td><input type="button" value="Daten &auml;ndern"></td>
+       <td><form action="signup_ok.php"><input type="submit" value="Daten absenden"></form></td>
+    </tr>
+  </table>
+</div>
+<p align="center" class="footer"><a href="../main.php"> home</a>| <a href="../show.php">texts</a> 
+  | <a href="../upload.php">upload</a> | <a href="../contact.php">contact</a> 
+  | <a href="../help.php">help</a><br>
+  <a href="login.php">login</a><br>
+  <br>
+  $Revision: 1.1 $ &copy; 2001 otmp-developer :)</p>
+</body>
+</html>
