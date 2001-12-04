28ebfd054d40ea799e90baec6eb8313b0c7f0f70
alexgn <alexgn>
2001-12-04 20:58:02 +0000

    Anpassen der Funktionen an unsere DB; Hinzufuegen weiterer Funktionen

--- Release1/lib/sql.php
+++ Release1/lib/sql.php
@@ -1,8 +1,8 @@
 <? 
 /*
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/Release1/lib/sql.php,v $
- * $Revision: 1.2 $
- * $Id: sql.php,v 1.2 2001/11/25 23:18:53 hifix Exp $
+ * $Revision: 1.3 $
+ * $Id: sql.php,v 1.3 2001/12/04 20:58:02 alexgn Exp $
  *
  * sql.php
  * This file stores all sql commands in functions.
@@ -34,16 +34,16 @@ function sql_LoginGetUserdata($username, $password) {
 }
 
 function sql_addNewUser($lastname, $firstname, $email, $username, $password) {
-/* insert an new user into the DB */
+/* insert a new user into the DB */
   global $CFG;
   $query = "INSERT INTO $CFG->tbl_user
-            (usrName, usrPasswd, usrEmail, usrFName, usrLName)
+            (LoginName, PersonPassword, PersonEmail, PersonName, PersonVorname)
             VALUES (
             '$username'
             ,PASSWORD('$password')
             ,'$email'
-            ,'$firstname'
             ,'$lastname'
+            ,'$firstname'
             )";
   $qid = db_query($query);
   return $qid;
@@ -52,7 +52,7 @@ function sql_addNewUser($lastname, $firstname, $email, $username, $password) {
 function sql_usernameExists($username) {
 /* returns 1 if the username exists, otherwise 0 */
   global $CFG;
-  $qid = db_query("SELECT 1 FROM $CFG->tbl_user WHERE usrName = '$username'");
+  $qid = db_query("SELECT 1 FROM $CFG->tbl_user WHERE LoginName = '$username'");
   return db_num_rows($qid);
 }
 
@@ -66,7 +66,7 @@ function sql_nameExists($firstname, $lastname) {
 function sql_emailExists($email) {
 /* returns 1 if the email address exists otherwise 0 */
   global $CFG;
-  $qid = db_query("SELECT 1 FROM $CFG->tbl_user WHERE usrEmail = '$email'");
+  $qid = db_query("SELECT 1 FROM $CFG->tbl_user WHERE PersonEmail = '$email'");
   return db_num_rows($qid);
 }
 
@@ -83,7 +83,7 @@ function sql_getUserdataFromUsername($username) {
 /* return the user (object) based on the username 
    user : Lastname, Firstname, LastName, Email */
   global $CFG;
-  $qid = db_query("SELECT usrFName as Firstname, usrLName as Lastname, usrEmail as Email FROM $CFG->tbl_user WHERE usrName = '$username'");
+  $qid = db_query("SELECT PersonVorname as Firstname, PersonName as Lastname, PersonEmail as Email FROM $CFG->tbl_user WHERE LoginName = '$username'");
   return db_fetch_object($qid);
 }
 
@@ -91,5 +91,57 @@ function sql_setUserpasswd($username, $passwd) {
 /* set new password for user with username */
   global $CFG;
   $qid = db_query("UPDATE $CFG->tbl_user SET usrPasswd = PASSWORD('$passwd') WHERE usrName = '$username'");
+}
+
+/* PerHatProg Table */
+
+function sql_getUserProgrammData($userid) {
+  global $CFG;
+  $qid = db_query("SELECT ProgrammName FROM $CFG->tbl_perhatprog WHERE PersonPID= '$userid'");
+  $i = 0;
+  while ($row = mysql_fetch_object ($qid)) {
+    $result[$i] = $row->ProgrammName;
+    $i = $i + 1;
+  }
+ return $result;
+}
+
+/* UebersetzerVonSprache Table */
+
+function sql_getUebersetzerVonSprache($userid) {
+  global $CFG;
+  $qid = db_query("SELECT t1.SpracheName FROM $CFG->tbl_sprache AS t1, $CFG->tbl_uebvonsprach AS t2 WHERE t2.UebersetzerUEID = '$userid' AND t1.SpracheAbkuerzung = t2.SpracheAbkuerzung");
+  $i = 0;
+  while ($row = mysql_fetch_object ($qid)) {
+    $result[$i] = $row->SpracheName;
+    $i = $i + 1;
+  }
+ return $result; 
+}
+
+/* UbersetzerNachSprache Table */
+
+function sql_getUebersetzerNachSprache($userid) {
+  global $CFG;
+  $qid = db_query("SELECT t1.SpracheName From $CFG->tbl_sprache AS t1, $CFG->tbl_uebnachsprach AS t2 WHERE t2.UebersetzerUEID = '$userid' AND t1.SpracheAbkuerzung = t2.SpracheAbkuerzung");
+   $i = 0;
+  while ($row = mysql_fetch_object ($qid)) {
+    $result[$i] = $row->SpracheName;
+    $i = $i + 1;
+  }
+ return $result; 
+}
+
+/* KenntSichAus Table */
+
+function sql_getUebersetzerKategorien($userid) {
+  global $CFG;
+  $qid = db_query("Select KategorieName FROM $CFG->tbl_kenntsichaus WHERE UebersetzerUEID = '$userid'");
+  $i = 0;
+  while ($row = mysql_fetch_object ($qid)) {
+    $result[$i] = $row->KategorieName;
+    $i = $i + 1;
+  }
+ return $result;
 }
 ?>
