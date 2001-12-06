49ac57a53fe55c80f4b60491b6ba1a253f23f3a4
ahlabadi <ahlabadi>
2001-12-06 22:14:35 +0000

    DB mit Kommentare

--- Release1/setup/tables.sql
+++ Release1/setup/tables.sql
@@ -5,6 +5,7 @@
 # ####################################
 
 
+
 DROP TABLE IF EXISTS otmp_Person ;
 DROP TABLE IF EXISTS otmp_Adresse ;
 DROP TABLE IF EXISTS otmp_Optionen ;
@@ -18,55 +19,69 @@ DROP TABLE IF EXISTS otmp_Konto ;
 DROP TABLE IF EXISTS otmp_SystemKonto ;
 DROP TABLE IF EXISTS otmp_BankKonto ;
 DROP TABLE IF EXISTS otmp_PerHatProg ;
-DROP TABLE IF EXISTS otmp_UebersetzerSprachen; 
+DROP TABLE IF EXISTS otmp_UebersetzerSprachen;
+DROP TABLE IF EXISTS otmp_KenntSichAus ;
+
+DROP TABLE IF EXISTS otmp_KatSpr; 
 DROP TABLE IF EXISTS otmp_PerPro;
+
+
 
 # #######################################
 # -------- TABELLEN  -------------------
 # #######################################
 
+# SMALLINT UNSIGNED  0.. 65535
+# MEDIUMINT UNSIGNED 0 .. 16777215
+# mit Admin wird ausgedruckt, ob diese Person ein Nutzer=0, ein Uebersetzer=1 oder 
+# Administrator > 1, Falls Admin Stufen gibt ?
+
+
 CREATE TABLE otmp_Person 
        ( 
 	 PersonPID		MEDIUMINT  UNSIGNED AUTO_INCREMENT	NOT NULL,	 
 	 PersonKennung		VARCHAR(20)	NOT NULL,
-	 PersonName		VARCHAR(50)	NOT NULL DEFAULT '',
-	 PersonVorname	     	VARCHAR(50), 
-	 PersonSex		ENUM('m','f') DEFAULT NULL, 
-	 PersonEmail		VARCHAR(255)  DEFAULT '', 
+	 PersonName		VARCHAR(20)	NOT NULL,
+	 PersonVorname	     	VARCHAR(20), 
+	 PersonSex		CHAR(1), 
+	 PersonEmail		VARCHAR(100), 
 	 PersonPassword		VARCHAR(20)	NOT NULL,
-	 PersonStatus		ENUM('locked','deleted')	DEFAULT NULL ,
-	 PersonAdminlevel	TINYINT UNSIGNED DEFAULT 0,
-	 PersonPunkte	 	SMALLINT(5) UNSIGNED DEFAULT 0,	
-	 PersonUebersetzer	TINYINT UNSIGNED DEFAULT 0, 
+	 PersonStatus		VARCHAR(20)	DEFAULT 'nicht gesperrt' ,
+	 PersonAdmin		CHAR(1)		DEFAULT	'0',
+	 PersonPunkte	 	SMALLINT(5) DEFAULT 0,	
+
 
 	 PRIMARY KEY(PersonPID)
 	 
        );
 
+
 # #######################################
  
-# ProgrammPacker 0 --> kein Packer
 
 CREATE TABLE otmp_Programm 
        ( 
 	 ProgrammPRGID	        SMALLINT(5) UNSIGNED AUTO_INCREMENT NOT NULL,
 	 ProgrammName		VARCHAR(20)	NOT NULL,
 	 ProgrammVersion	VARCHAR(20)	NOT NULL, 
-	 ProgrammPacker		ENUM('0','1') DEFAULT '0',
 
 	PRIMARY KEY(ProgrammPRGID)
        );
 
+
+
 # #####################################
+
 
 CREATE TABLE otmp_Filetype 
        ( 
 	 FiletypeFID		SMALLINT(5) UNSIGNED AUTO_INCREMENT NOT NULL,
 	 FiletypePRGID		SMALLINT(5) UNSIGNED NOT NULL,
-	 FiletypeType		VARCHAR(5) NOT NULL DEFAULT '---',
+	 FiletypeFileExt	VARCHAR(5) NOT NULL DEFAULT 'txt',
 
 	PRIMARY KEY(FiletypeFID)
        );
+
 
 # ######################################
 
@@ -80,7 +95,12 @@ CREATE TABLE otmp_PerPro
 	PRIMARY KEY(PerProPRGID,PerProPID)
        );
 
+
+
+
 # ######################################
+
+
 
 CREATE TABLE otmp_Adresse 
        ( 
@@ -93,20 +113,26 @@ CREATE TABLE otmp_Adresse
 	
        );
 
+
+
 # #######################################
+
 
 CREATE TABLE otmp_Optionen 
        ( 
          OptionenPID		MEDIUMINT UNSIGNED NOT NULL,
 	 OptionenSprachGUI	VARCHAR(20),
-	 OptionenPublicEmail	ENUM('y','n') DEFAULT 'n', 
+	 OptionenPublicEmail	CHAR(1) DEFAULT 'N', 
 	 OptionenSecureGUI	VARCHAR(20), 
 	 OptionenMutterSprache	VARCHAR(20), 
 
 	PRIMARY KEY(OptionenPID)
        );
 
+
+
 # #######################################
+
 
 CREATE TABLE otmp_Sprache
        ( 
@@ -118,17 +144,39 @@ CREATE TABLE otmp_Sprache
 
        );
 
+
 # #####################################
+
+
 
 CREATE TABLE otmp_Kategorie 
        ( 
-	 KategorieKID      	TINYINT(3) UNSIGNED NOT NULL,
-	 KategorieName		VARCHAR(60)	NOT NULL,
+	 KategorieKID      	TINYINT(3) UNSIGNED AUTO_INCREMENT NOT NULL,
+	 KategorieName		VARCHAR(20)	NOT NULL,
+	 KategorieSprache	VARCHAR(20)	NOT NULL,
 
 	PRIMARY KEY(KategorieKID)
        );
 
 # #####################################
+
+# KatSpr = Relationship zwischen Kategorie und Sprache
+
+CREATE TABLE otmp_KatSpr 
+       ( 
+	 KatSprKID      	TINYINT(3) UNSIGNED NOT NULL,
+	 KatSprSID		TINYINT(3) UNSIGNED NOT NULL,
+
+	PRIMARY KEY(KatSprKID,KatSprSID)
+       );
+
+
+
+
+
+
+
+# ####################################
 
 # OTID = Original Text ID
 # NTID = neue Text ID
@@ -140,10 +188,11 @@ CREATE TABLE otmp_Auftrag
 	 AuftragAID		MEDIUMINT  UNSIGNED AUTO_INCREMENT	NOT NULL,	 
 	 AuftragOTID		MEDIUMINT  UNSIGNED NOT NULL DEFAULT 0,	 
 	 AuftragNTID		MEDIUMINT  UNSIGNED NOT NULL DEFAULT 0,	 
-	 AuftragStatus	ENUM('open','finished','work','hold','deleted') NOT NULL DEFAULT 'open',
+	 AuftragStatus	ENUM('open','finished','in work','on hold','deleted') NOT NULL DEFAULT 'open',
 	 AuftragDatum		DateTIME NOT NULL DEFAULT '0000-00-00 00:00:00'  , 
 	 AuftragNID	MEDIUMINT UNSIGNED NOT NULL,
 	 AuftragUEID	MEDIUMINT UNSIGNED NOT NULL DEFAULT 0, 
+
 	 AuftragTransDatum	DATE DEFAULT '0000-00-00', 
 	 AuftragBisDatum	DATE DEFAULT '0000-00-00', 
 
@@ -152,41 +201,62 @@ CREATE TABLE otmp_Auftrag
 
        );
 
+
+
+
 # #####################################
 
 # OTID = Original Text ID
+
 
 CREATE TABLE otmp_Text 
        ( 
 	 TextTID		MEDIUMINT  UNSIGNED AUTO_INCREMENT	NOT NULL,	 
 	 TextOTID		MEDIUMINT  UNSIGNED NOT NULL DEFAULT 0,	 
 	 TextTitel		VARCHAR(200) NOT NULL,
-	 TextAbstract		TINYTEXT,
+	 TextAbstract		VARCHAR(255),
+	 TextFileExt		VARCHAR(5) NOT NULL DEFAULT 'txt',
 	 TextDatum		Datetime NOT NULL DEFAULT '0000-00-00 00:00:00'  , 
 	 TextLaenge		MEDIUMINT  UNSIGNED DEFAULT 0,	 
-	 TextStatus		ENUM('open','finished','work') NOT NULL DEFAULT 'open',
+	 TextStatus		ENUM('open','finished','in work') NOT NULL DEFAULT 'open',
 	 TextSID	        TINYINT(3) UNSIGNED NOT NULL DEFAULT 0, 		 
 	 TextKID      		TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
 	 TextFID		SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0,
 	 TextAutor		MEDIUMINT  UNSIGNED NOT NULL DEFAULT 0,	 
+	 TextPfad		VARCHAR(50) NOT NULL DEFAULT '--',	
 
 	 PRIMARY KEY (TextTID) 
 	) ;
 
+
+
+
+
+
+
+
+
 # ###################################
+
 
 CREATE TABLE otmp_Konto
        ( 
     	 KontoPID		MEDIUMINT  UNSIGNED NOT NULL,	 
-	 KontoKontoNr		INT(15) UNSIGNED NOT NULL, 
+	 KontoKontoNr		INT(15)		NOT NULL, 
 	 KontoName		VARCHAR(20),
 	 KontoType		VARCHAR(20),
+
+
 
 	PRIMARY KEY(KontoPID)
        );
 
+
+
+
 # ###################################
  
+
 CREATE TABLE otmp_SystemKonto 
        ( 
 	 SystemKontoPID		MEDIUMINT  UNSIGNED NOT NULL,	 
@@ -197,6 +267,9 @@ CREATE TABLE otmp_SystemKonto
 
        );
 
+
+
+
 # #####################################
 
 CREATE TABLE otmp_BankKonto 
@@ -204,11 +277,14 @@ CREATE TABLE otmp_BankKonto
 	 BankKontoPID		MEDIUMINT  UNSIGNED NOT NULL,	 
 	 BankKontoBank		VARCHAR(30), 
 	 BankKontoBLZ		VARCHAR(20)	NOT NULL,
-	 BankKontoKontoNr	INT(15)	UNSIGNED NOT NULL,
+	 BankKontoKontoNr	INT(15)		NOT NULL,
 	
 	PRIMARY KEY(BankKontoKontoNr,BankKontoBLZ)
 
        );
+
+
+
 
 # #######################################################
 # #######################################################
@@ -218,6 +294,8 @@ CREATE TABLE otmp_BankKonto
 # #######################################################
 # #######################################################
 
+
+
 CREATE TABLE otmp_PerHatProg 
        ( 
 	PerHatProgPID		MEDIUMINT  UNSIGNED NOT NULL,	 
@@ -226,30 +304,33 @@ CREATE TABLE otmp_PerHatProg
 	PRIMARY KEY(PerHatProgPID,PerHatProgPRGID)
        );
 
+
+
+
 # ##############################################################
 
-# UebersetzerSprachenAuto, zeigt ob dieser Eintrag automatisch (-->1) oder vom Uebersetzer
-# eingetragen wurde (0)
 
-CREATE TABLE otmp_UebersetzerSprachen
+
+CREATE TABLE otmp_KenntSichAus
        ( 
-	UebersetzerSprachenUEID		MEDIUMINT  UNSIGNED NOT NULL,	 
-	UebersetzerSprachenVonSID	TINYINT(3) UNSIGNED NOT NULL, 	        
-	UebersetzerSprachenNachSID	TINYINT(3) UNSIGNED NOT NULL, 	
-	UebersetzerSprachenKID		TINYINT(3) UNSIGNED NOT NULL,
-	UebersetzerSprachenAuto		ENUM('0','1') DEFAULT '0',
-	
-	PRIMARY KEY(UebersetzerSprachenUEID,UebersetzerSprachenVonSID,UebersetzerSprachenNachSID,UebersetzerSprachenKID)
-
+	KenntSichAusUEID	MEDIUMINT  UNSIGNED NOT NULL,	 
+        KenntSichAusKID		TINYINT(3) UNSIGNED NOT NULL DEFAULT '--'
+	 
        );
 
 
 
+# ##############################################################
 
 
 
-
-
+CREATE TABLE otmp_UebersetzerSprachen
+       ( 
+	UebersetzerSprachenUEID		MEDIUMINT  UNSIGNED NOT NULL,	 
+	UebersetzerSprachenVonSID	        TINYINT(3) UNSIGNED NOT NULL, 	        
+	UebersetzerSprachenNachSID	        TINYINT(3) UNSIGNED NOT NULL 	
+	
+       );
 
 
 
