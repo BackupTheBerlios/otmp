63ee960236758d927378031e8f33b4bd684caa64
hifix <hifix>
2001-12-09 21:17:52 +0000

    functional for release 1

--- Release1/docs/upload.php
+++ Release1/docs/upload.php
@@ -3,12 +3,14 @@
  * Dokument Upload
  *
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/Release1/docs/upload.php,v $
- * $Revision: 1.1 $
- * $Id: upload.php,v 1.1 2001/12/04 23:57:35 hifix Exp $
+ * $Revision: 1.2 $
+ * $Id: upload.php,v 1.2 2001/12/09 21:17:52 hifix Exp $
  *
  * To Do:
  * - LOcalisation
- * 
+ * - Screen, wenn nicht eingeloggt
+ * - Fileupload implementation
+ * - Filetyp (now dummy)
  */
 
 /******************************************************************************
@@ -16,6 +18,7 @@
  *****************************************************************************/
 
 include("../application.php");
+checklogin();
 
 /* form has been submitted, try to create the new user account */
 if (match_referer() && isset($HTTP_POST_VARS)) {
@@ -25,7 +28,7 @@ if (match_referer() && isset($HTTP_POST_VARS)) {
   if (empty($errormsg)) {
     $status = upload_file($frm);
 
-    $DOC_TITLE = "Signup Successful";
+    $DOC_TITLE = "Upload Successful";
     include("$CFG->templatedir/header.php");
     include("templates/upload_success.inc");
     include("$CFG->templatedir/footer.php");
@@ -62,21 +65,33 @@ function validate_form(&$frm, &$errors) {
     $msg .= "<li>Sie haben die Sprache für den Text nicht angegeben";
   }
   
+  if (empty($frm["cat"])) {
+      $errors->cat = true;
+      $msg .= "<li>Sie haben keine Textkategorie angegeben";
+  }
+  
+  /*
   if (empty($frm["file"])) {
     $errors->file = true;
     $msg .= "<li>Sie haben keine Datei angegeben/ausgewählt";
   }
-
-  if (empty($frm["filetype"])) {
-     $errors->filetype = true;
+  */
+  if (empty($frm["filetyp"])) {
+     $errors->filetyp = true;
      $msg .= "<li>Sie haben kein Dateiformat angegeben";
   }
   return $msg;
 }
 
 function upload_file(&$frm) {
-  
+  global $session;
   // wie handelt man denn ein per form geschicktes File ?
+  
+  // neuen Text anlegen
+  // FileTypID ist falsch gesetzt (dummy)
+  $ntextid = sql_addNewText($frm['title'],nvl($frm['abstract']),nvl($frm['length'],0),$frm['lang'],nvl($frm['cat'],0),nvl($frm['filetyp'],0),$session['userid']); 
+  
   return 1;
 }
+
 ?>
