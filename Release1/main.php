2a5b7eb41696a59d02fbc4bac5b912590c3bafc3
darkpact <darkpact>
2001-12-14 09:36:11 +0000

    Neu Designed

--- Release1/main.php
+++ Release1/main.php
@@ -1,63 +1,53 @@
 <? /*
     * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/Release1/main.php,v $
-    * $Revision: 1.7 $
-    * $Id: main.php,v 1.7 2001/12/10 22:06:21 darkpact Exp $
+    * $Revision: 1.8 $
+    * $Id: main.php,v 1.8 2001/12/14 09:36:11 darkpact Exp $
     */
 
 include "application.php";
 
-$DOC_TITLE = "Main";
+$DOC_TITLE = "Hauptseite";
 
 include("$CFG->templatedir/header.php");
 ?>
-<!-- doc body -->
-<table width="100%">
+
+<table width="100%" cellpadding="10">
   <tr>
-    <td width="70%">
-      <h3>Willkommen bei OTMP - Ihrem &Uuml;bersetzungsdienst!</font></h3>
-
-      <p>OTMP ist eine offene &Uuml;bersetzungsplattform. Auf OTMP können sie Ihre
-      Texte veröffentlichen und in verschiedene andere Sprachen &uuml;bersetzen
-      lassen.</p>
-
-      <p>Texte bezeichnet hierbei sowohl Artikel, Ausarbeitungen, Bücher, Skripte,
-      Prosa, Diplomarbeiten usw. als auch einfache Schriften, Liebesbriefe, und mehr.</p>
-
-      <p>Wenn sie diesen Service auch nutzen m&ouml;chten, so m&uuml;ssen sie sich
-      hierf&uuml;r anmelden. Die Anmeldung ist kostenlos und ihre Daten werden nicht
-      an andere Personen weitergegeben. Nachdem sie sich angemeldet haben, k&ouml;nnen
-      sie sowohl Texte für &Uuml;bersetzungen ablegen als auch welche &Uuml;bersetzen.</p>
-
-      <p>OTMP ist auf gute &Uuml;bersetzer angewiesen, und daher stets auf der Suche
-      nach neuen &Uuml;bersetzern. Lesen sie weiter, wenn sie wissen wollen, welche
-      Vorteile ihnen das Arbeiten als &Uuml;bersetzer bringt.</p>
-
-      <p>Wir w&uuml;nschen Ihnen viel Spass und Erfolg mit unserem System!</p>
+    <td width="65%" rowspan="3">
+    <? include("$CFG->templatedir/intro.inc"); ?>
     </td>
-    <td valign="top">
-      <br>
-      <ul>
-        <li><a href="<?=$CFG->wwwroot?>/help/what.html">Was ist OTMP ?</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/help/faq.html">H&auml;ufig gestellte Fragen (FAQ)</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/help/help.html">Weitere Hilfen und Kontakt</a></li>
-      </ul>
-      <ul>
-        <li><a href="<?=$CFG->wwwroot?>/user/signup.php">Neu Anmelden</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/user/login.php">Einloggen</a></li>
-      </ul>
-      <ul>
-        <li><a href="<?=$CFG->wwwroot?>/docs/documentlist.php?list=all">Das gesammte Textverzeichnis anzeigen</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/docs/search.php?list=all">Das Textverzeichnis durchsuchen</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/docs/upload.php">Einen Text Übersetzen lassen</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/docs/tasks.php">Einen Text Übersetzen wollen</a></li>
-        <li><a href="<?=$CFG->wwwroot?>/user/user_data.php">Ihre Daten</a></li>
-      </ul>
+    <td bgcolor="#CCCCCC">
+      <h5>Benutzer</h5>
+      <p> <a href="<?=$CFG->wwwroot?>/user/signup.php">Neu Anmelden</a>
+    <? if ( isset($session['userid']) ){ ?>
+      <br><a href="<?=$CFG->wwwroot?>/user/logout.php">Ausloggen</a>
+    <? } else { ?>
+      <br><a href="<?=$CFG->wwwroot?>/user/login.php">Einloggen</a>
+    <? } ?>
+      <br><a href="<?=$CFG->wwwroot?>/user/user_data.php">Ihre Daten</a>
+      </p>
+    </td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC">
+      <h5>Dokumente</h5>
+      <p> <a href="<?=$CFG->wwwroot?>/docs/tasklist.php">Ich will &Uuml;bersetzen</a>
+      <br><a href="<?=$CFG->wwwroot?>/docs/upload.php">Ich brauche eine &Uuml;bersetzung</a>
+      <br><a href="<?=$CFG->wwwroot?>/docs/search.php?list=all">Ich suche ein Dokument</a>
+      </p>
+    </td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC">
+      <h5>Informationen</h5>
+      <p> <a href="<?=$CFG->wwwroot?>/help/what.php">Was ist OTMP?</a>
+      <br><a href="<?=$CFG->wwwroot?>/help/faq.php">H&auml;ufig gestellte Fragen (FAQ)</a>
+      <br><a href="<?=$CFG->wwwroot?>/help/help.php">Weitere Hilfen und Kontakt</a>
+      </p>
     </td>
   </tr>
 </table>
-<!-- doc body end -->
 
-<!-- doc footer -->
 <?
 include("$CFG->templatedir/footer.php");
 ?>
