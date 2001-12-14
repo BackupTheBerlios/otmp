559f3cfebb9da97fc69217b68dd8a89c34641d71
darkpact <darkpact>
2001-12-14 09:34:57 +0000

    Anpassung der Beispile an die tables 1.14 und das neue Sprachverzeichnis

--- Release1/setup/examples.sql
+++ Release1/setup/examples.sql
@@ -1,12 +1,19 @@
 #
 # Beispielsql für OTMP
-# $Id: examples.sql,v 1.4 2001/12/13 21:49:41 hifix Exp $
-# $Revision: 1.4 $
+# $Id: examples.sql,v 1.5 2001/12/14 09:34:57 darkpact Exp $
+# $Revision: 1.5 $
 #
 # ----------------------------------------------------------------------
 # Log for examples.sql
 # ----------------------------------------------------------------------
 # $Log: examples.sql,v $
+# Revision 1.5  2001/12/14 09:34:57  darkpact
+# Anpassung der Beispile an die tables 1.14 und das neue Sprachverzeichnis
+#
+# Revision 1.5  2001/12/13 21:49:41  darkpact
+# Aufträge Korrigiert wegen verkürzter Auftragtabelle
+# Texte Korrigiert wegen neuem Inhalt der Sprachtabelle
+#
 # Revision 1.4  2001/12/13 21:49:41  hifix
 # Sprachen jetzt wirklich drin
 #
@@ -28,17 +35,17 @@
 # Dumping data for table `otmp_Auftrag`
 #
 
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (1, 1, 3, 'work', '2001-12-01 23:52:48', 1, 3, '0000-00-00', '2001-12-08');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (2, 1, 4, 'open', '2001-12-01 23:52:48', 1, 0, '0000-00-00', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (3, 1, 5, 'open', '2001-12-01 23:52:48', 1, 0, '0000-00-00', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (4, 1, 6, 'open', '2001-12-01 23:52:48', 1, 0, '0000-00-00', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (5, 2, 7, 'open', '2001-12-01 00:02:19', 3, 4, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (6, 1, 8, 'finished', '2001-12-01 00:02:19', 3, 4, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (7, 9, 10, 'finished', '2001-12-09 00:02:19', 4, 4, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (8, 9, 11, 'work', '2001-12-09 00:02:19', 4, 1, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (9, 9, 12, 'work', '2001-12-09 00:02:19', 4, 1, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (10, 9, 13, 'open', '2001-12-09 00:02:19', 4, 0, '2001-12-07', '0000-00-00');
-INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragStatus, AuftragDatum, AuftragNID, AuftragUEID, AuftragTransDatum, AuftragBisDatum) VALUES (11, 9, 14, 'open', '2001-12-09 00:02:19', 4, 0, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (1, 1, 3, '2001-12-01 23:52:48', 1, '0000-00-00', '2001-12-08');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (2, 1, 4, '2001-12-01 23:52:48', 1, '0000-00-00', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (3, 1, 5, '2001-12-01 23:52:48', 1, '0000-00-00', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (4, 1, 6, '2001-12-01 23:52:48', 1, '0000-00-00', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (5, 2, 7, '2001-12-01 00:02:19', 3, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (6, 1, 8, '2001-12-01 00:02:19', 1, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (7, 9, 10, '2001-12-09 00:02:19', 4, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (8, 9, 11, '2001-12-09 00:02:19', 4, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (9, 9, 12, '2001-12-09 00:02:19', 4, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (10, 9, 13, '2001-12-09 00:02:19', 4, '2001-12-07', '0000-00-00');
+INSERT INTO otmp_Auftrag (AuftragAID, AuftragOTID, AuftragNTID, AuftragDatum, AuftragNID, AuftragTransDatum, AuftragBisDatum) VALUES (11, 9, 14, '2001-12-09 00:02:19', 4, '2001-12-07', '0000-00-00');
 
 #
 # Dumping data for table `otmp_BankKonto`
@@ -310,20 +317,20 @@ INSERT INTO otmp_Sprache (SpracheSID, SpracheName, SpracheAbkuerzung, SpracheSor
 # Dumping data for table `otmp_Text`
 #
 
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (1, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'finished', '4', '0', 0, 1);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (2, 2, 'Solidarit&auml;tsverhalten der DDR gegenüber Nicaragua', '', '2001-12-01 23:42:46', 1000, 'finished', '1', '0', 0, 1);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (3, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-04 23:42:46', 0, 'work', '5', '0', 0, 3);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (4, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '2', '0', 0, 0);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (5, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '1', '0', 0, 0);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (6, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '3', '0', 0, 0);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (7, 2, 'Solidarit&auml;tsverhalten der DDR gegenüber Nicaragua', '', '2001-12-07 23:42:46', 875, 'open', '1', '0', 0, 0);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (8, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-08 23:42:46', 0, 'finished', '6', '0', 0, 4);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (9, 9, 'Universal DCI Tournament Rules', '', '2001-12-08 03:42:46', 0, 'finished', '2', '0', 0, 4);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (10, 9, 'Allgemeine DCI-Veranstaltungsregeln', '', '2001-12-09 03:42:46', 0, 'finished', '1', '0', 0, 4);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (11, 9, 'Universal DCI Tournament Rules', '', '2001-12-10 03:42:46', 0, 'work', '3', '0', 0, 1);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (12, 9, 'Universal DCI Tournament Rules', '', '2001-12-10 03:42:46', 0, 'work', '4', '0', 0, 1);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (13, 9, 'Universal DCI Tournament Rules', '', '2001-12-11 03:42:46', 0, 'open', '6', '0', 0, 0);
-INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (14, 9, 'Universal DCI Tournament Rules', '', '2001-12-12 03:42:46', 0, 'open', '9', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (1, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'finished', '64', '0', 0, 1);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (2, 2, 'Solidarit&auml;tsverhalten der DDR gegenüber Nicaragua', '', '2001-12-01 23:42:46', 1000, 'finished', '64', '0', 0, 1);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (3, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-04 23:42:46', 0, 'work', '72', '0', 0, 3);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (4, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '123', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (5, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '141', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (6, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-01 23:42:46', 0, 'open', '39', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (7, 2, 'Solidarit&auml;tsverhalten der DDR gegenüber Nicaragua', '', '2001-12-07 23:42:46', 875, 'open', '90', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (8, 1, 'Biologischer Anbau von Tee in Angola', '', '2001-12-08 23:42:46', 0, 'finished', '60', '0', 0, 4);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (9, 9, 'Universal DCI Tournament Rules', '', '2001-12-08 03:42:46', 0, 'finished', '72', '0', 0, 4);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (10, 9, 'Allgemeine DCI-Veranstaltungsregeln', '', '2001-12-09 03:42:46', 0, 'finished', '64', '0', 0, 4);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (11, 9, 'Universal DCI Tournament Rules', '', '2001-12-10 03:42:46', 0, 'work', '61', '0', 0, 1);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (12, 9, 'Universal DCI Tournament Rules', '', '2001-12-10 03:42:46', 0, 'work', '122', '0', 0, 1);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (13, 9, 'Universal DCI Tournament Rules', '', '2001-12-11 03:42:46', 0, 'open', '140', '0', 0, 0);
+INSERT INTO otmp_Text (TextTID, TextOTID, TextTitel, TextAbstract, TextDatum, TextLaenge, TextStatus, TextSID, TextKID, TextFID, TextAutor) VALUES (14, 9, 'Universal DCI Tournament Rules', '', '2001-12-12 03:42:46', 0, 'open', '99', '0', 0, 0);
 
 #
 # Dumping data for table `otmp_UebersetzerSprachen`
