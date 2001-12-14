28530b1656bb9bc68c98e69755bf445061cc2c07
darkpact <darkpact>
2001-12-14 09:33:04 +0000

    Da Neudesign

--- Release1/docs/documentlist.php
+++ Release1/docs/documentlist.php
@@ -1,8 +1,8 @@
 <?
 /*
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/otmp/Repository/Release1/docs/documentlist.php,v $
- * $Revision: 1.5 $
- * $Id: documentlist.php,v 1.5 2001/12/10 22:08:46 darkpact Exp $
+ * $Revision: 1.6 $
+ * $Id: documentlist.php,v 1.6 2001/12/14 09:33:04 darkpact Exp $
  *
  * To Do:
  * - Localisation
@@ -15,8 +15,6 @@
 include("../application.php");
 
 $DOC_TITLE = "Document Info";
-
-setdefault($list,"all");
 
 include("$CFG->templatedir/header.php");
 include("templates/documentlist.inc");
